af7c9b2ea22087e7ea1ebc4439df6497e6116103
wevans311082 <wayne.edward.evans@googlemail.com>
2026-04-11 20:56:57 +0100

    roll back

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -4,8 +4,4 @@
 
 import { io } from 'socket.io-client';
 
-export const socket = io('/', {
-	path: '/socket.io',
-	withCredentials: true,
-	transports: ['polling', 'websocket']
-});
+export const socket = io();
